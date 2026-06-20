fa8c068b25fa8e5f43ff9316556323e6c17d5c33
yxzhangcas <yxzhangcas@outlook.com>
2026-06-20 18:00:11 +0800

    [p02] add test_program and fix bug

---
+++ p02-test-your-program/sample/programs/sample/tests/program_test.rs
@@ -0,0 +1,74 @@
+use anchor_lang::system_program;
+use litesvm::LiteSVM;
+use solana_sdk::{
+    instruction::{AccountMeta, Instruction},
+    pubkey::Pubkey,
+    signature::{Keypair, Signer},
+};
+use solana_transaction::Transaction;
+
+use sample;
+
+#[test]
+fn test_program() {
+    // Initialize the test environment
+    let mut svm = LiteSVM::new();
+
+    // Deploy your program to the test environment
+    let program_id = Pubkey::from(sample::ID);
+    let program_bytes = include_bytes!("../../../target/deploy/sample.so");
+    let _ = svm.add_program(program_id, program_bytes);
+
+    // Create and fund test accounts
+    let payer = Keypair::new();
+    svm.airdrop(&payer.pubkey(), 10_000_000_000).unwrap();
+
+    // 1. init poll
+    let discriminator = [193, 22, 99, 197, 18, 33, 115, 117]; // from idl
+    let poll_id: u64 = 10086;
+    let start_time: u64 = 0;
+    let end_time: u64 = 1000_000;
+    let name = b"poll 10086";
+    let description = b"poll 10086 description";
+
+    let mut data: Vec<u8> = Vec::new();
+    data.extend_from_slice(&discriminator);
+    data.extend_from_slice(&poll_id.to_le_bytes());
+    data.extend_from_slice(&start_time.to_le_bytes());
+    data.extend_from_slice(&end_time.to_le_bytes());
+    data.extend_from_slice(&(name.len() as u32).to_le_bytes());
+    data.extend_from_slice(name);
+    data.extend_from_slice(&(description.len() as u32).to_le_bytes());
+    data.extend_from_slice(description);
+
+    let poll_account =
+        Pubkey::find_program_address(&[b"poll", &poll_id.to_le_bytes()], &sample::ID).0;
+
+    // Create your instruction
+    let instruction = Instruction {
+        program_id,
+        accounts: vec![
+            AccountMeta::new(payer.pubkey(), true),
+            AccountMeta::new(poll_account, false),
+            AccountMeta::new_readonly(system_program::ID, false),
+        ],
+        data,
+    };
+
+    // Build transaction
+    let tx = Transaction::new_signed_with_payer(
+        &[instruction],
+        Some(&payer.pubkey()),
+        &[&payer],
+        svm.latest_blockhash(),
+    );
+
+    // Send transaction
+    let result = svm.send_transaction(tx).unwrap();
+
+    // Check transaction succeeded
+    println!("Transaction logs: {:?}", result.logs);
+
+    println!("Program executed successfully!");
+
+}
